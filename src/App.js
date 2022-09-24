d82477f508c722707da08b14127af93a56c5b24f
AmmarElsherif2021 <ammar.alshareef2014@gmail.com>
2022-09-24 03:25:33 +0200

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     
     <Routes>
       
-      <Route path="/" element= { <Layout/> }>
+      <Route path="/"  element= { <Layout/> }>
        <Route index element={<Home/>}/>
        <Route path="/about"  element= { <About/> }></Route>
        <Route path="/contact"  element= { <Contact/> }></Route>
